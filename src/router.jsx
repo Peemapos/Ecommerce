f50e15469bdfb9e0cdffcc488e8331bb62875781
Peemapos <peemapos@gmail.com>
2024-10-27 22:19:15 +0700

    Initial commit

--- src/router.jsx
+++
@@ -1,8 +0,0 @@
-import {createBrowserRouter} from "react-router-dom";
-import App from './App.jsx';
-import Product from './components/product.jsx'
-
-export const router = createBrowserRouter ([
-    {path: "/", element: <App />},
-    {path: "/product", element: <Product />}
-  ]);
